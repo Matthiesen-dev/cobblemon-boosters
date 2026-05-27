3a15b41dd9740638cba76452260ae084b3c1dc7e
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-27 13:48:37 -0700

    Add additional mod properties to resource processing

--- gradle/build-logic/src/main/kotlin/boosters.platform-resources-conventions.gradle.kts
+++ gradle/build-logic/src/main/kotlin/boosters.platform-resources-conventions.gradle.kts
@@ -8,5 +8,11 @@ plugins {
 tasks.named<ProcessResources>("processResources") {
     inputs.property("mod_id", project.property("mod_id").toString())
     inputs.property("version", project.version)
+    inputs.property("mod_name", project.property("mod_name").toString())
+    inputs.property("mod_description", project.property("mod_description").toString())
+    inputs.property("mod_license", project.property("mod_license").toString())
+    inputs.property("mod_author", project.property("mod_author").toString())
+    inputs.property("github_url", project.property("github_url").toString())
+    inputs.property("modrinth_url", project.property("modrinth_url").toString())
 }
 
