1e6d3e4bf98aef5efc6bc5809a6f345bb1b4bd22
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-28 09:10:00 -0700

    Refactor permission handling to use PermissionLevel enum in PermissionsConfig

--- common/src/main/java/dev/matthiesen/common/cobblemon_boosters/Constants.java
+++ common/src/main/java/dev/matthiesen/common/cobblemon_boosters/Constants.java
@@ -15,24 +15,6 @@ public class Constants {
             "shiny"
     );
 
-    public enum PERMISSION_LEVELS {
-        NONE(0),
-        SPAWN_PROTECTION_BYPASS(1),
-        CHEAT_COMMANDS_AND_COMMAND_BLOCKS(2),
-        MULTIPLAYER_MANAGEMENT(3),
-        ALL_COMMANDS(4);
-
-        private final int level;
-
-        PERMISSION_LEVELS(int level) {
-            this.level = level;
-        }
-
-        public int getLevel() {
-            return level;
-        }
-    }
-
     public static Logger LOGGER = LogManager.getLogger(ModName);
 
     public static void createInfoLog(String message) {
--- common/src/main/java/dev/matthiesen/common/cobblemon_boosters/config/PermissionsConfig.java
+++ common/src/main/java/dev/matthiesen/common/cobblemon_boosters/config/PermissionsConfig.java
@@ -3,7 +3,7 @@ package dev.matthiesen.common.cobblemon_boosters.config;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
-import dev.matthiesen.common.cobblemon_boosters.Constants;
+import dev.matthiesen.common.matthiesen_lib_api.permission.PermissionLevel;
 
 public class PermissionsConfig {
     @SerializedName("permissionlevels")
@@ -12,79 +12,79 @@ public class PermissionsConfig {
     public static class PermissionLevels {
         @SerializedName("command.boosters.reload")
         public int RELOAD_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.catch")
         public int CATCH_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.catch.start")
         public int CATCH_START_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.catch.stop")
         public int CATCH_STOP_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.catch.status")
         public int CATCH_STATUS_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.experience")
         public int EXPERIENCE_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.experience.start")
         public int EXPERIENCE_START_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.experience.stop")
         public int EXPERIENCE_STOP_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.experience.status")
         public int EXPERIENCE_STATUS_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.shiny")
         public int SHINY_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.shiny.start")
         public int SHINY_START_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.shiny.stop")
         public int SHINY_STOP_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.shiny.status")
         public int SHINY_STATUS_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.bucket")
         public int BUCKET_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.bucket.start")
         public int BUCKET_START_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.bucket.stop")
         public int BUCKET_STOP_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.bucket.status")
         public int BUCKET_STATUS_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
 
         @SerializedName("command.boosters.clear_queues")
         public int CLEAR_QUEUES_PERMISSION =
-                Constants.PERMISSION_LEVELS.ALL_COMMANDS.getLevel();
+                PermissionLevel.ALL_COMMANDS.getNumericalValue();
 
         @SerializedName("command.boosters.check_queue")
         public int CHECK_QUEUE_PERMISSION =
-                Constants.PERMISSION_LEVELS.NONE.getLevel();
+                PermissionLevel.NONE.getNumericalValue();
     }
 
     @SuppressWarnings("unused")
